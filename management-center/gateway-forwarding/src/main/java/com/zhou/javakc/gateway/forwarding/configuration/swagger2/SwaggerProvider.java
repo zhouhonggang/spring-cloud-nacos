727b3776e19e0e7f1e1d58140f5717b6c0563c8d
zhouhonggang <honggang_z@yeah.net>
2020-04-30 08:49:22 +0800

    更新项目语言

--- management-center/gateway-forwarding/src/main/java/com/zhou/javakc/gateway/forwarding/configuration/swagger2/SwaggerProvider.java
+++ management-center/gateway-forwarding/src/main/java/com/zhou/javakc/gateway/forwarding/configuration/swagger2/SwaggerProvider.java
@@ -34,14 +34,25 @@ public class SwaggerProvider implements SwaggerResourcesProvider {
         List<SwaggerResource> resources = new ArrayList<>();
         List<String> routes = new ArrayList<>();
         //取出gateway的route
-        routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
+        routeLocator.getRoutes().subscribe(route -> {
+            if(!route.getId().equals("security-oauth2") || !route.getId().equals("github_route"))
+            {
+                routes.add(route.getId());
+            }
+        });
         //结合配置的route-路径(Path)，和route过滤，只获取有效的route节点
         gatewayProperties.getRoutes().stream().filter(routeDefinition -> routes.contains(routeDefinition.getId()))
                 .forEach(routeDefinition -> routeDefinition.getPredicates().stream()
                         .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
-                        .forEach(predicateDefinition -> resources.add(swaggerResource(routeDefinition.getId(),
-                                predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
-                                        .replace("/**", API_URI)))));
+                        .forEach(predicateDefinition -> {
+                            if(!routeDefinition.getId().equals("security-oauth2"))
+                            {
+                                resources
+                                        .add(swaggerResource(routeDefinition.getId(),
+                                                predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
+                                                .replace("/**", API_URI)));
+                            }
+                        }));
         return resources;
     }
 
